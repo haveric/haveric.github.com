d233aa614675227d87592cffbb09f33ba8f3976a
Ryan Breuer <breuerryan@gmail.com>
2014-08-23 17:39:18 -0500

    Add per sound volume

--- projects/ld30/v1/js/application/audio.js
+++ projects/ld30/v1/js/application/audio.js
@@ -3,8 +3,8 @@ var SoundManager = function() {
     this.volume = 1;
 }
 
-SoundManager.prototype.add = function(name, src) {
-    var sound = new Sound(name, src);
+SoundManager.prototype.add = function(name, src, vol) {
+    var sound = new Sound(name, src, vol);
     this.sounds.push(sound);
 }
 
@@ -15,7 +15,7 @@ SoundManager.prototype.play = function(audioName) {
         var sound = self.sounds[i];
         if (sound.name === audioName) {
             var audio = new Audio(sound.src);
-            audio.volume = self.volume;
+            audio.volume = self.volume * sound.vol;
             audio.play();
             
             return;
@@ -35,13 +35,14 @@ SoundManager.prototype.getVolume = function() {
     return volume;
 }
 
-var Sound = function (name, src) {
+var Sound = function (name, src, vol) {
     this.name = name;
     this.src = src;
+    this.vol = vol || 1;
 }
 
 var soundManager = new SoundManager();
 
 soundManager.add('jump', 'assets/jump.wav');
 soundManager.add('land', 'assets/land.wav');
-soundManager.add('attack', 'assets/attack.wav');
+soundManager.add('attack', 'assets/attack.wav', .15);
