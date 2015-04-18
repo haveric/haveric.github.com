798d472d3a655a70db7a23bf55637f02c2d66049
Ryan Breuer <breuerryan@gmail.com>
2015-04-18 09:51:42 -0500

    More randomness to stars

--- projects/ld32/js/application/star.js
+++ projects/ld32/js/application/star.js
@@ -1,11 +1,15 @@
 var PI_2 = 2 * Math.PI;
 var Star = function(y) {
     this.x = Math.random() * 800;
-    this.y = y || -50;
+    
     
     var weighted = Math.random() * 10;
     if (weighted < 7) {
         this.radius = Math.random() * 1.5;
+    } else if (weighted > 9.95 && weighted <= 9.99) {
+        this.radius = (Math.random() * 25) + 25;
+    } else if (weighted > 9.99) {
+        this.radius = (Math.random() * 250) + 75;
     } else {
         this.radius = (Math.random() * 2) + 1;
     }
@@ -13,7 +17,13 @@ var Star = function(y) {
     if (this.radius < 1) {
         this.radius = 1;
     }
+    
+    this.y = y || -50 - this.radius*2;
     this.velocity = this.radius * 0.8;
+    
+    if (this.velocity > 10) {
+        this.velocity = 10;
+    }
     
     this.color = "#ffffff";
     this.generateRandomColor(64, 196, 64);
@@ -22,17 +32,16 @@ var Star = function(y) {
 Star.prototype.move = function(index) {
     this.y += this.velocity;
     
-    if (this.y >= CANVAS_HEIGHT + 50) {
+    if (this.y >= CANVAS_HEIGHT + 50 + this.radius*2) {
         killStar(index);
     }
 }
 
 Star.prototype.draw = function(context, frame) {
-    context.fillStyle= this.color;
+    context.fillStyle = this.color;
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, PI_2);
     context.fill();
-    context.stroke();
 }
 
 Star.prototype.generateRandomColor = function(mixR, mixG, mixB) {
