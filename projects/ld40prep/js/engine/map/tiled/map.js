34dd7454c91cc0d14e246960477723c76c6f6f59
Ryan Breuer <breuerryan@gmail.com>
2017-12-01 18:18:52 -0600

    Slight improvement in map rendering

--- projects/ld40prep/js/engine/map/tiled/map.js
+++ projects/ld40prep/js/engine/map/tiled/map.js
@@ -15,32 +15,41 @@ MapTiled.prototype.getTile = function(x,y) {
     return this.tiles[x][y];
 }
 MapTiled.prototype.generate = function() {
-    var unvisited = [];
-    
     for (var i = 0; i < this.rows; i++) {
         for (var j = 0; j < this.cols; j++) {
             this.tiles[i][j] = new Path();
         }
     }
-    
-    var startX = Math.floor(this.rows / 2);
-    var startY = Math.floor(this.cols / 2);
 }
 
 MapTiled.prototype.draw = function(context, frame, x, y) {
-    var left = x - 12;
+    var initialLeft = x - 12;
+    var initialTop = y - 10;
+    var left = initialLeft;
     var right = x + 13;
-    var top = y - 10;
+    var top = initialTop;
     var bot = y + 10;
-    
+
+    if (left < 0) {
+        left = 0;
+    }
+
+    if (right >= this.rows) {
+        right = this.rows;
+    }
+
+    if (top < 0) {
+        top = 0;
+    }
+
+    if (bot >= this.cols) {
+        bot = this.cols;
+    }
+
     for (var i = left; i < right; i++) {
         for (var j = top; j < bot; j++) {
-            if (i < 0 || j < 0 || i >= this.rows || j >= this.cols) {
-                // draw nothing
-            } else {
-                var tile = this.tiles[i][j];
-                tile.draw(context, frame, i-left, j-top, this, i, j);
-            }
+            var tile = this.tiles[i][j];
+            tile.draw(context, frame, i-initialLeft, j-initialTop, this, i, j);
         }
     }
 }
